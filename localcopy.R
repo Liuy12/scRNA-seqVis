f814a9265610b1da963560a96287ac7424a90e0e
Liuy12 <yuanhangliu.ok@gmail.com>
2024-12-30 16:24:00 -0600

    misc

--- localcopy.R
+++ localcopy.R
@@ -1,23 +1,25 @@
-cranpacks <- c('devtools','shiny', 'shinydashboard', 'magrittr', 'shinythemes', 'colorspace', 'doParallel', 'ggplot2', 'scatterplot3d', 'GGally', 'data.table', 'dplyr', 'threejs', 'd3heatmap', 'metricsgraphics', 'networkD3', 'DT', 'statmod', 'igraph', 'LPCM')
+cranpacks <- c('devtools','shiny', 'shinydashboard', 'magrittr', 'shinythemes', 'colorspace', 'doParallel', 'ggplot2', 'scatterplot3d', 'GGally', 'data.table', 'dplyr', 'threejs', 'metricsgraphics', 'networkD3', 'DT', 'statmod', 'igraph', 'LPCM')
 biocpacks <- c('RUVSeq', 'DESeq', 'DESeq2', 'XBSeq', 'edgeR', 'limma', 'scde', 'monocle', 'EBSeq', 'NetSAM', 'MAST', 'ROTS')
 
-library(BiocInstaller)
+library(BiocManager)
 for(i in cranpacks){
   if(! i %in% installed.packages()[,1])
     install.packages(i)
 }
 for(i in biocpacks){
   if(! i %in% installed.packages()[,1])
-    biocLite(i)
+    BiocManager::install(i)
 }
 
 if(!"slidify" %in% installed.packages()[,1])
-  devtools::install_github("slidify", "ramnathv")
+  devtools::install_github("ramnathv/slidify")
 if(!'slidifyLibraries' %in% installed.packages()[,1])
-  devtools::install_github("slidifyLibraries", "ramnathv")
+  devtools::install_github("ramnathv/slidifyLibraries")
 if(!'rCharts' %in% installed.packages()[,1])
-  devtools::install_github("rCharts", "ramnathv")
+  devtools::install_github("ramnathv/rCharts")
 if(!'BPSC' %in% installed.packages()[,1])
-  devtools::install_github("BPSC","nghiavtr")
+  devtools::install_github("nghiavtr/BPSC")
 if(!'rga' %in% installed.packages()[,1])
-  devtools::install_github("rga", "skardhamar")
+  devtools::install_github("skardhamar/rga")
+if(!'d3heatmap' %in% installed.packages()[,1])
+  devtools::install_github("talgalili/d3heatmap")
